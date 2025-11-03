8361d36a8703bd86729e643478caa9c52595b946
jpaul-kr <jonpk555@gmail.com>
2025-11-03 17:16:59 +0100

    funciona mas o menos

--- srcs/containers/backend/volume-backend/getNumber.js
+++ srcs/containers/backend/volume-backend/getNumber.js
@@ -1,3 +1,7 @@
 export const getNumber = (req, res) => {
-    res.send(JSON.stringify({result: 42}));
-  }
+  res.writeHead(200, {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': 'http://localhost:8080',
+  });
+  res.end(JSON.stringify({result: 42}));
+};
--- srcs/containers/frontend/volume-frontend/index.js
+++ srcs/containers/frontend/volume-frontend/index.js
@@ -1,9 +1,11 @@
 
+const apiUrl = 'http://localhost:3000';
+
 async function main() {
     const top = document.createElement('div');
     top.textContent = "Volume Frontend Loaded";
     const main = document.createElement('div');
-    const data = await fetch('http://backend/get-number');
+    const data = await fetch(`${apiUrl}/get-number`);
     const number = await data.json().result;
     main.textContent = `Number from backend: ${number}`;
     document.body.appendChild(top);
